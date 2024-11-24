a15a4be6ddd8a82087ff6c66b1b78fc52fb2edf7
Rohit Reddy Gangwar <rohitreddygangwar@gmail.com>
2024-11-25 00:26:30 +0530

    temp

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -58,7 +58,7 @@ export const api = {
 			headers: {
 				"Content-Type": "application/json",
 			},
-			body: JSON.stringify({ email, host: location.host }),
+			body: JSON.stringify({ email }),
 		});
 		const data = await res.json();
 		return data;
