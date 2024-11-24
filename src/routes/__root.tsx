aad9dd906aafcb09c10950e9036ebeac0daeb393
Rohit Reddy Gangwar <rohitreddygangwar@gmail.com>
2024-11-25 02:08:28 +0530

    temp

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,19 @@
-import * as React from 'react'
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { NavBar } from "../components/nav-bar";
 
 export const Route = createRootRoute({
-  component: RootComponent,
-})
+	component: RootComponent,
+});
+const queryClient = new QueryClient();
 
 function RootComponent() {
-  return (
-    <React.Fragment>
-      <div>Hello "__root"!</div>
-      <Outlet />
-    </React.Fragment>
-  )
+	return (
+		<QueryClientProvider client={queryClient}>
+			<div className="relative grid min-h-screen w-full bg-white p-4">
+				<NavBar />
+				<Outlet />
+			</div>
+		</QueryClientProvider>
+	);
 }
